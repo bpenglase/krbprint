e9dad4635f1c18b1e89d121a14fe910f5aad77a5
Brandon Penglase <bp-gh@spaceservices.net>
2012-07-30 15:20:23 -0400

    v0.4 upload

--- krbprint.sh
+++ krbprint.sh
@@ -2,30 +2,28 @@
 ##################
 # Kerberos printing script for PCT
 # Used in combination with LaunchD to monitor the printers.conf
-# and when a user adds a printer, this removes the auth requirement
+# and when a user adds a printer, this modifies the auth requirement
 # Created By: Brandon Penglase
 # Creation Date: 09/17/09
-# Modified On: 10/19/10
+# Modified On: 07/30/12
 # Modified By: Brandon Penglase
 # ChangeLog:
 #	0.1: Inital Release
 #	0.2: Updated to look for the smb symlink, if it's not there, create it. 
 #	0.3: Updated to use sed file inline editing, instead of creating a new file
 #		and having to move that file back into place. 
+#	0.4: Updated to remove ksmbprintd references, and use Apple's built in
+#		method. This involves removing the check for the symlink, and instead
+#		of deleting the AuthInfoRequired line, replace it with negotiate.
+#		Also change the check from AuthInfoRequired (as it will be needed
+#		to username,password, as thats what we need to replace). 
 ##################
 DATE=`date "+%m%d%y-%H%M%S"`
 
-# Check to see if /usr/libexec/cups/backend/smb is a symlink, if not, correct it.
-if [ ! -L /usr/libexec/cups/backend/smb ]; then
-	# It isn't, so fix that
-	echo ${DATE} "- Fixed SMB Symlink" >> /var/log/krbprint.log
-	mv /usr/libexec/cups/backend/smb /usr/libexec/cups/backend/smb-orig
-	ln -s /usr/local/bin/ksmbprintspool /usr/libexec/cups/backend/smb
-fi
-
-if grep -q "AuthInfoRequired" /etc/cups/printers.conf; then
+# Check to see if it's set for user
+if grep -q "username,password" /etc/cups/printers.conf; then
         launchctl stop org.cups.cupsd
-        sed -i bak -e '/AuthInfoRequired/d' /etc/cups/printers.conf 
+        sed -i bak -e 's/username,password/negotiate/g' /etc/cups/printers.conf 
         launchctl start org.cups.cupsd
         echo ${DATE} "- Modified printers.conf" >> /var/log/krbprint.log
 else
